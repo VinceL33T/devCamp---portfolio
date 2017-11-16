fb3860ee4a8c7a9529f9c1c3923a713f02aff19c
Vince Greco <vincegreco@Vinces-MacBook-Air.local>
2017-11-15 21:55:54 -0700

    Implemented

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -52,13 +52,23 @@ class BlogsController < ApplicationController
   # DELETE /blogs/1
   # DELETE /blogs/1.json
   def destroy
-    @blog.destroy
-    respond_to do |format|
+    @blog.destroy    respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Post was removed' }
       format.json { head :no_content }
     end
   end
 
+def toggle_status
+  if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    
+  redirect_to blogs_url, notice: 'Post status has been updated.'
+end
+  
+end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
@@ -69,4 +79,4 @@ class BlogsController < ApplicationController
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
-end
+
