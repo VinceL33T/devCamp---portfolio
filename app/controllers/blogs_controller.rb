d21cf0a82968503148f15effa687453957763849
L33t <vincegreco92@gmail.com>
2017-12-04 20:36:51 -0700

    added custom layours for blogs and portfolios

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,13 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  layout "blog"
 
+  
   # GET /blogs
   # GET /blogs.json
   def index
     @blogs = Blog.all
-    @page_title = "My Portolio Blog"
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
