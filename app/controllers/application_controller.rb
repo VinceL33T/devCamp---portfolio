4d6dc65f7b97a8986c986557a968bd3d19994d57
L33t <vincegreco92@gmail.com>
2017-12-10 14:12:48 -0700

    copy right

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,17 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
 
+  				def set_copyright
+  				@copyright = DevcampViewTool::Renderer.copyright "Vince Greco", "All rights reserved"
 end
+end
+
+module DevcampViewTool
+	class Renderer 
+			def self.copyright name, msg 
+				"&copy: #{Time.now.year} | <b> #{name}</b> #{msg}".html_safe
+		end
+			end
+				end
